6e0fbe9e9a81bde2edde3c39b880f6ff1a49d56b
nodealvan <167424793+nodealvan@users.noreply.github.com>
2024-07-15 00:29:20 +0800

    first commit

---
+++ download_cv.js
@@ -0,0 +1,16 @@
+    document.addEventListener('DOMContentLoaded', function () {
+      const downloadButton = document.querySelector('.button');
+
+      downloadButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const url = downloadButton.getAttribute('href');
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'Gianell-Cv.pdf');
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    });
